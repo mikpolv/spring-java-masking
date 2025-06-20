cfad5e9b075e72b4811359b1eab201088ec40edc
Mikhail Polivanov <mpolivanov@vtb.ru>
2025-06-20 11:40:36 +0300

    init

---
+++ src/main/java/ru/astondev/masking/MaskingApplication.java
@@ -0,0 +1,13 @@
+package ru.astondev.masking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaskingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/astondev/masking/MaskingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astondev.masking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaskingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
